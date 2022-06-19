6c8821c38de356c50f9686b06fae82e36995eece
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-19 14:59:46 +0100

    Add all cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { single } from "../assets";
+import { double, single, triple } from "../assets";
 
 const Cards = () => {
   return (
@@ -12,6 +12,42 @@ const Cards = () => {
             alt="/"
           />
           <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+          <p className="text-center text-4xl font-bold">$150</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500Gb Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 5 Tb</p>
+          </div>
+          <button className="bg-turboGreen w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+            Start Trial
+          </button>
+        </div>
+
+        <div className="w-full shadow-xl flex flex-col md:my-0 p-4 my-8 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={double}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Double User</h2>
+          <p className="text-center text-4xl font-bold">$150</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500Gb Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 5 Tb</p>
+          </div>
+          <button className="bg-black w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-turboGreen">
+            Start Trial
+          </button>
+        </div>
+
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={triple}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Triple User</h2>
           <p className="text-center text-4xl font-bold">$150</p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500Gb Storage</p>
