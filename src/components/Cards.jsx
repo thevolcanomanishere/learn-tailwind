ac85c905725bcd7f1e57994f93673e39884b547c
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-19 14:55:52 +0100

    Added cards section styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,22 +4,24 @@ import { single } from "../assets";
 const Cards = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
-      <div className="max-w-[1240] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={single}
             alt="/"
           />
-          <h2 className="text-2xl">Single User</h2>
-          <p>$150</p>
-          <div>
-            <p>500Gb Storage</p>
-            <p>1 Granted User</p>
-            <p>Send up to 5 Tb</p>
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+          <p className="text-center text-4xl font-bold">$150</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500Gb Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 5 Tb</p>
           </div>
+          <button className="bg-turboGreen w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+            Start Trial
+          </button>
         </div>
-        <button>Start Trial</button>
       </div>
     </div>
   );
